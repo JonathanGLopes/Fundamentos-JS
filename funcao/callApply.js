493894ffac96b9e07f03c170d75fa7f49c1e772a
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-04-28 16:10:21 -0300

    Function Call and Apply

---
+++ funcao/callApply.js
@@ -0,0 +1,24 @@
+// A diferença dessas duas funções é a forma de como você passa os parâmetros para chamada da função
+// No caso do Call nós passamos os parâmetros diretamente dentro da função
+// No Apply nós passamos dentro de um Array
+
+function getPreco(imposto = 0, moeda = 'R$'){
+    return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
+}
+
+const produto = {
+    nome: 'Notebook',
+    preco: 4589,
+    desc: 0.15,
+    getPreco
+}
+
+console.log(produto.getPreco())
+
+const carro = {preco: 49990, desc: 0.20}
+
+console.log(getPreco.call(carro))
+console.log(getPreco.apply(carro))
+
+console.log(getPreco.call(carro, 0.17, '$'))
+console.log(getPreco.apply(carro, [0.17, '$']))
