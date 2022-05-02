cbfb7ef7da51dae447d91c04964b68989fd8bc55
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-05-02 18:50:41 -0300

    Criando objetos

---
+++ Objeto/criandoObjeto.js
@@ -0,0 +1,49 @@
+//formas de criação de um objeto em JS
+
+//usando a notação literal
+const obj1 = {
+    //conteúdo aqui
+}
+
+//Object em JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+
+//Função construtora
+function produto(nome, preco, desc){
+    this.nome = nome
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    }
+}
+
+//Neste exemplo apenas o nome está visível fora do objeto e o preco e desconto estão encapsulados
+
+const p1 = new produto('Caneta', 7.99, 0.15)
+const p2 = new produto('Notebook', 2999.99, 0.25)
+
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+//Função Factory
+function criarFuncionario(nome, salarioBase, faltas){
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return(salarioBase/ 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario('Joao', 7980, 4)
+const f2 = criarFuncionario('Maria', 8490, 1)
+
+console.log(f1.getSalario(), f2.getSalario())
+
+
+//Object.create
+
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
