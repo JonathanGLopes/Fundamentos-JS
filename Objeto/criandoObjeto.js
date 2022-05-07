0d90177d7f93ac8156dd83a2d9a3e43cb92fc644
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-05-07 17:30:37 -0300

    crinado objeto

--- Objeto/criandoObjeto.js
+++ Objeto/criandoObjeto.js
@@ -47,3 +47,7 @@ console.log(f1.getSalario(), f2.getSalario())
 const filha = Object.create(null)
 filha.nome = 'Ana'
 console.log(filha)
+
+//Uma função famosa que retorna um objeto..
+const fromJSON =  JSON.parse('{"info": "Sou um JSON"}');
+console.log(fromJSON.info)
