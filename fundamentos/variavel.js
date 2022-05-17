ddae087ef409b35c59b46cad5e27036c310275cc
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-05-17 19:45:37 -0300

    declaração de variável

---
+++ fundamentos/variavel.js
@@ -0,0 +1,8 @@
+const nome = 'Jonathan'; // Criando uma variável e atribuindo um valor a ela
+console.log(nome)
+
+let teste; // Criando uma variável apenas para reservar um nome e atribuir o valor a ela depois
+console.log(teste);
+
+teste = 'Jonathan Lopes';
+console.log(teste);
