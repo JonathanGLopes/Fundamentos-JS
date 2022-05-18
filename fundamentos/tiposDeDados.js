1a0335049f5bc35a8f6c216581e979ee09483f95
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-05-18 17:25:23 -0300

    tipos de dados primitivos

---
+++ fundamentos/tiposDeDados.js
@@ -0,0 +1,15 @@
+// Tipos de dados primitivos
+
+const nome = 'Jonathan'; // String
+const nome1 = 'Jonathan Lopes'; // String
+
+const num1 = 10; // Number
+const num2 = 10.52; // Number 
+
+let nomeAluno; // Undefined -> Não aponta pra local nenhum na memória
+let sobrenomeAluno = null; // Nulo -> Não aponta pra local nenhum na memória
+
+const aprovado = true; // Boolean = verdadeiro ou falso
+const reprovado = false;
+
+console.log(typeof nome); // typeof é utilizado para verificar o tipo da variável. Se quiser verificar o tipo e o valor basta colocar  (typeof nome, nome)
