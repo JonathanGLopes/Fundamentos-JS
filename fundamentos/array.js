f53a8fb7751a72c9c341a3ee3d340de6f8c70069
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-05-28 08:40:06 -0300

    Mais exemplos de array

--- fundamentos/array.js
+++ fundamentos/array.js
@@ -1,4 +1,16 @@
 // Array é uma forma de agrupar multiplos valores numa única estrutura. O conteúdo de uma array fica dentro de colchetes [] e separado por vírgula.
+//                 0       1        2
+const alunos = ['Luiz', 'Maria', 'João'];
+console.log(alunos); // podemos buscar pelo array completo utilizando o nome da variável 
+console.log(alunos[0]); // ou buscar pelo indice do array
+console.log(alunos[2])
 
-const valores = [7.7, 8.9, 6.3, 9.2];
-console.log(valores);
+console.log(alunos.length); // Buscando o tamanho do array
+
+alunos.push('Fernanda'); // Adicionando novo item ao fim array
+alunos.unshift('Marcos'); // Adicionando ao inicio do array
+console.log(alunos);
+
+// alunos.pop(); // pop remove o ultimo elemento do array
+// alunos.shift(); // shift remove o primeiro elemento do array
+// console.log(alunos);
