e325bc0d5b735d1f6d5ae04ab866e4e2e8c6d0ef
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-06-28 17:31:13 -0300

    For basico

---
+++ logica/for.js
@@ -0,0 +1,17 @@
+const frutas = ['Maça', 'Laranja', 'Melão', 'Uva', 'Abacaxi', 'Limão'];
+
+
+// Na condição frutas.length, estamos usando o tamanho do array como uma condição
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
+
+// console.log("linha 1");
+// console.log("linha 2");
+// console.log("linha 3");
+// console.log("linha 4");
+// console.log("linha 5");
+
+for (let i = 0; i <= 5; i ++) {
+    console.log(`Linha ${i}`) 
+}
