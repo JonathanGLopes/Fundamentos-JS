fa47db0b31802760dc46aaf5b785d2d94cc9f61c
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-08-01 17:47:01 -0300

    string e template string

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -1,5 +1,16 @@
-const escola = "Cod3r";
-
-console.log('Escola '.concat(escola).concat("!"));
 
 // String é uma cadeia de caracteres delimitada por aspas simples ou duplas
+
+const nome = 'Jonathan';
+const idade = 26;
+const ano = 2022;
+
+const apresentacao = "Eu sou " + nome +"," + " tenho " + idade + " anos " + "e estamos no ano de " + ano;
+
+console.log(apresentacao);
+
+
+// Aqui estamos utilizando template string
+const apresentacao2 = `Eu sou ${nome}, tenho ${idade} anos e estamos no ano de ${ano}`;
+
+console.log(apresentacao2)
