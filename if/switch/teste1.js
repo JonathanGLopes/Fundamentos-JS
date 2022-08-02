0cd9bd3da2ff6dd5eaf33451ba4158eb0bc29ad6
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-08-02 18:28:18 -0300

    Mais um exemplo utilizando if

--- if/switch/teste1.js
+++ if/switch/teste1.js
@@ -1,3 +1,5 @@
+//Essa parte é chamada de estrutura de controle
+
 const idade = 15;
 
 if (idade >= 18){
@@ -5,4 +7,15 @@ if (idade >= 18){
 } else {
     const anosFaltando = 18 - idade;
     console.log(`Matheus é muito novo para dirigir, aguarde ${anosFaltando} anos.`);
+}
+
+
+const peso = 70;
+const altura = 1.85;
+const IMC = peso / (altura ** 2);
+
+if (IMC <= 22){
+    console.log("Está tudo okay");
+} else {
+    console.log("Acima do peso");
 }
