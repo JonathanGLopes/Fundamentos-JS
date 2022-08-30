22b5c954a44a44ac527d1ab6b0489a157cfb9299
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-08-30 17:46:50 -0300

    IIFE

--- funcao/IIFE.js
+++ funcao/IIFE.js
@@ -1,9 +1,20 @@
 // IIFE -> Immediately Invoked Function Expression
 // A função se torna uma expressão que é imediatamente executada. A variável definida dentro da expressão não pode ser acessada fora de seu escopo.
-
+// Assim sendo protegida do escopo global
 // Sintaxe
 
-(function(){
-    console.log("Será executado na hora!");
-    console.log("Foge do escopo mais abrangente");
-})();
+(function(idade, altura){
+
+    const sobrenome = 'Lopes';
+    function criaNome(nome){
+        return nome + ' ' + sobrenome;
+    }
+
+    function falaNome(){
+        console.log(criaNome('Jonathan'));
+    }
+
+    falaNome();
+    console.log(idade, altura);
+
+})(26, 1.80);
