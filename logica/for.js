f8817f092584c24a5cdd834b514ce3e9cc8e8d92
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-08-31 17:54:36 -0300

    exemplos com o for

--- logica/for.js
+++ logica/for.js
@@ -18,3 +18,23 @@ for (let i = 0; i <= 5; i ++) {
     console.log(`Linha ${i}`) 
 }
 
+
+// Calculando idade utilizando o loop
+const anos = [1995, 1990, 1998, 2000];
+const idade = [];
+
+for (let i = 0; i < anos.length; i++){
+    idade.push(2022 - anos[i]);
+}
+
+console.log(idade);
+
+// Utilizando continue
+const testeArray = ['Jonathan', 'Lopes', 'Developer', 26, 2022, 'JavaScript', 'Coding'];
+console.log("-----SOMENTE STRINGS-----");
+// Neste exemplo utilizando o continue, o loop irá ignorar tudo que não for uma string e imprimir no console.
+for( let i = 0; i < testeArray.length; i++){
+    if(typeof testeArray[i] !== 'string') continue;
+
+    console.log(testeArray[i], typeof testeArray);
+}
