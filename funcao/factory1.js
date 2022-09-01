5c78d538da3748b1be006bf974fc8e3f68255103
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-09-01 18:19:33 -0300

    function factory

--- funcao/factory1.js
+++ funcao/factory1.js
@@ -1,19 +1,29 @@
 // Function factory é uma função que retorna um objeto
+// Quando uma função está dentro de um objeto nós a chamamos de função de método
 
-
-function criarPessoa(){
+function criarPessoa(nome, sobrenome, altura, peso){
     return {
-        nome: 'Ana',
-        sobrenome: 'Silva'
-    }
+        nome,
+        sobrenome,
+        fala: function(assunto){
+            return `${nome} está ${assunto}.`;
+        },
+        altura: altura,
+        peso: peso,
+        imc(){
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
+
+    };
 }
 
-console.log(criarPessoa())
+const p1 = criarPessoa('Jonathan', 'Lopes', 1.8, 60);
+console.log(p1.imc());
+console.log(p1.fala('falando sobre JS'));
 
 
-/*
-    Neste segundo exemplo nós personalizamos os dados a partir dos parametros.
-*/
+
 
 function criarProduto(nome, preco){
     return {
