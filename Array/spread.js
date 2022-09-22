e4d23c9d5f96a5eb0e0138e5af4b0ec50682de3b
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-09-22 16:01:42 -0300

    operador spread

---
+++ Array/spread.js
@@ -0,0 +1,29 @@
+// Spread Operator
+
+let primeiro = [1, 2, 3, 4, 5];
+
+let segundo = [primeiro, 6, 7, 8, 9, 10];
+
+// Desta maneira nós conseguimos unir as informações dos 2 arrays, mas como podemos ver no console, fica um array dentro do outro e não todas informações juntas em um unico array.
+console.log(segundo);
+
+// Dessa forma usando o Spread Operator, nós usamos os ... antes da variável do array, e todas informações dela vão ser unidas ao outro formando assim um único array.
+let segundoOperator = [...primeiro, 6, 7, 8, 9, 10];
+console.log(segundoOperator);
+
+
+const pessoa = {
+    nome: 'Jorge',
+    sobrenome: 'Menezes',
+    idade: 43,
+}
+
+const info = {
+    ...pessoa,
+    cargo: 'Gerente de projetos',
+    codigo: 536597,
+    telefone: 1155555555
+}
+
+// O mesmo funciona com objetos, como podemos ver que o console devolveu as informações de ambos objetos apenas no info, também contendo as informações do pessoa
+console.log(info);
