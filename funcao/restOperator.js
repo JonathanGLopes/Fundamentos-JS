b24f5cd37952132c32f13532fceb4cfa9b778309
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-09-24 16:18:27 -0300

    Exemplo utilizando rest operator

---
+++ funcao/restOperator.js
@@ -0,0 +1,11 @@
+// Ele funciona muito bem quando não sabemos a quantidade de parâmetros que a função irá receber, como no exemplo abaixo.
+
+function sorteador(...numeros){
+    console.log(numeros);
+
+    const numeroGerado = Math.floor(Math.random() * numeros.length);
+
+    console.log(`Numero gerado foi: ${numeros[numeroGerado]}`); //Acessando através da posição deles no array para não vir um número fora dos que foram passados como argumento.
+}
+
+sorteador(1, 4, 22, 38, 51, 68, 97, 88, 60);
