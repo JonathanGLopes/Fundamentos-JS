26fccb7b7f86fe0b9ca7ec3b3a245273759ba346
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-10-01 19:57:54 -0300

    selecionando os elementos

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const btnAgain = document.querySelector(".again");
+const guessNumber = document.querySelector(".number");
+const btnCheck = document.querySelector(".check");
+const input = document.querySelector(".guess");
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".highscore");
