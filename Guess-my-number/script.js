ef5f0f46f830e6db6179eeebb9c3380981e66a0f
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-10-01 22:08:55 -0300

    capturando click e input vazio

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -3,6 +3,15 @@
 const btnAgain = document.querySelector(".again");
 const guessNumber = document.querySelector(".number");
 const btnCheck = document.querySelector(".check");
-const input = document.querySelector(".guess");
 const score = document.querySelector(".score");
 const highScore = document.querySelector(".highscore");
+const message = document.querySelector(".message");
+
+btnCheck.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const input = Number(document.querySelector(".guess").value);
+
+    if(!input){
+        message.textContent = "❌ No number!"
+    }
+});
