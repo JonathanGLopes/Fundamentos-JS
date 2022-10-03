f0088199da57f7fab9c6b334595d8af7c8edb6b7
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-10-03 20:11:01 -0300

    iniciando a logica

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -7,11 +7,23 @@ const score = document.querySelector(".score");
 const highScore = document.querySelector(".highscore");
 const message = document.querySelector(".message");
 
+
+const secretNumber = Math.trunc(Math.random() * 20) +1;
+const inicialScore = 20;
+guessNumber.textContent = secretNumber;
+
 btnCheck.addEventListener("click", (e)=>{
     e.preventDefault();
     const input = Number(document.querySelector(".guess").value);
 
     if(!input){
         message.textContent = "❌ No number!"
+        
+    } else if (input === secretNumber){
+        message.textContent = "🎉 Correct Number!"
+    } else if(input > secretNumber){
+        message.textContent = "📈 Too high!";
+    } else if(input < secretNumber){
+        message.textContent = "📉 Too low!";
     }
 });
