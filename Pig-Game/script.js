3ea07d674d0401145ba8a8a8607f6d4bfd1c90c5
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-10-20 17:04:09 -0300

    alternando entre os jogadores

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
 const current0El = document.querySelector("#current--0");
 const current1El = document.querySelector("#current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -14,7 +17,9 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice funcionality
 btnRoll.addEventListener("click", function() {
@@ -30,9 +35,15 @@ btnRoll.addEventListener("click", function() {
     if(dice !== 1){
         // Add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
+
     }
 
 });
