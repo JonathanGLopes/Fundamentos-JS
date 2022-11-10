5373efbc84f4c9d7409b9d576b5fa3f329d28289
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-11-09 23:18:54 -0300

    metodos de array pt 4

---
+++ Array/metodosArray4.js
@@ -0,0 +1,5 @@
+// Concat - Método utilizado para concatenação de 2 arrays
+const filhos = ['Marcos',  'Ugo'];
+const filhas = ['Brenda','Aline'];
+const todosFilhos = filhos.concat(filhas, 'Paulo');
+console.log(todosFilhos);
