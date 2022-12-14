f7a3de39b015e39978ca13cebbf29e1955426572
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-12-14 20:04:23 -0300

    organizando arquivos

--- fundamentos/templateString.js
+++ fundamentos/templateString.js
@@ -7,7 +7,7 @@ const template = `Olá ${nome}!`
 console.log(concatenacao, template)
 
 //expressões
-console.log(`1 + 1 = $ {1 + 1}`)
+console.log(`1 + 1 = ${1 + 1}`)
 
 const up = texto => texto.toUpperCase();
 console.log(`Ei...${up('cuidado')}!`)
