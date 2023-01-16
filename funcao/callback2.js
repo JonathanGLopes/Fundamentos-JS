e4288de4f7c5e08b6d45562fc91a38d4a1e27075
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-01-16 14:40:26 -0300

    exercicio callback

--- funcao/callback2.js
+++ funcao/callback2.js
@@ -1,41 +1,28 @@
-function rand(min = 1000, max = 3000) {
-    const num = Math.random() * (max - min) +
-      min;
-    return Math.floor(num);
-  }
-  
-  function f1(callback) {
-    setTimeout(function() {
-      console.log('f1');
-      if (callback) callback();
-    }, rand());
-  }
-  
-  function f2(callback) {
-    setTimeout(function() {
-      console.log('f2');
-      if (callback) callback();
-    }, rand());
-  }
-  
-  function f3(callback) {
-    setTimeout(function() {
-      console.log('f3');
-      if (callback) callback();
-    }, rand());
-  }
-  
-  f1(f1Callback);
-  
-  function f1Callback() {
-    f2(f2Callback);
-  }
-  
-  function f2Callback() {
-    f3(f3Callback);
-  }
-  
-  function f3Callback() {
-    console.log('Olá mundo!');
-  }
-  
+function newButton(text, callback){
+    const $div = document.querySelector(".callback");
+    const $button = document.createElement("button");
+    $button.textContent = text;
+
+    callback($button);
+
+    $div.insertAdjacentElement("beforeend", $button);
+
+    return $button;
+}
+
+newButton("Login", (button) =>{
+    button.style.cssText = `
+      font-size: 20px;
+      padding: 10px 20px;
+      margin-right: 6px;
+    `;
+});
+
+
+newButton("Inscreva-se", (button) => {
+    button.style.cssText = `
+      font-size: 20px;
+      padding: 10px 20px;
+      margin-left: 6px;
+    `;
+});
