a7f6a0ddd0f933fa61adae37f968ef148b123c5d
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-01-25 17:06:56 -0300

    método splice

--- Array/metodosArray.js
+++ Array/metodosArray.js
@@ -12,7 +12,8 @@ pilotos.unshift('Leclerc'); //Adiciona no indice 0
 console.log(pilotos);
 
 // Splice pode adicionar ou remover elementos
-
+// pilotos.splice(índice atual, delete, elemento1, elemento2, etc..);
+// Utilizando índice negativo, o -1 corresponde ao ultimo índice, -2 o penultimo e assim por diante.
 //Adicionando
 pilotos.splice(2,0, 'Bottas', 'Massa');
 console.log(pilotos);
@@ -20,6 +21,10 @@ console.log(pilotos);
 //Removendo
 pilotos.splice(3, 1);
 console.log(pilotos);
+
+//Removendo com indice negativo
+const removido = pilotos.splice(-1, 1);
+console.log(`O item ${removido} foi removido com o splice`);
 
 const algunsPilotos = pilotos.slice(2) // Retorna um novo array a partir de terminado indice
 console.log(algunsPilotos);
