d2821f9122cbe66f03e5b008a20a7543ada8d450
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-02-01 19:01:23 -0300

    exercicio map

--- Array/map.js
+++ Array/map.js
@@ -42,3 +42,23 @@ const novoId = pessoas.map(function(objeto, indice){
 });
 
 console.log(novoId)
+
+
+const meuArray = [
+    [10, 20],
+    [30, 40],
+    [50, 60],
+    [70, 80],
+    [90, 100],
+];
+
+const novoArray = meuArray.map(function(item){
+    let soma = 0;
+    item.forEach(function(subItem){
+        soma += subItem;
+    });
+
+    return soma;
+});
+
+console.log(novoArray);
