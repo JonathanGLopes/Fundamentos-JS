f69e5a7ff4fd27a63370470b96145cf3ce3a7d4b
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-02-10 19:40:19 -0300

    Mais exemplos com prototype

--- Objeto/prototype.js
+++ Objeto/prototype.js
@@ -22,3 +22,22 @@ Pessoa.prototype.nomeCompleto = function() {
 const pessoa1 = new Pessoa('Jonathan', 'Lopes');
 
 console.log(pessoa1);
+
+
+
+function Produto(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual){
+    this.preco = this.preco - (this.preco * (percentual / 100));
+}
+
+Produto.prototype.aumento = function(percentual){
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+const p1 = new Produto('Camiseta', 70);
+p1.desconto(50);
+console.log(p1);
