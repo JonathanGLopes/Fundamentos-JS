7a3f659ffefcb9673e1bdfbe96bb60775c4cb684
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-03-07 20:51:38 -0300

    método estático e instânciado

--- Classe/metodos.js
+++ Classe/metodos.js
@@ -16,10 +16,17 @@ class Carro {
     parar() {
       console.log(`O ${this.modelo} agora parou de se mover.`);
     }
+
+    // O método estático está associado a classe em si ao invés de estar associado a uma instância específica da classe.
+    // É o mesmo que executar uma função dentro da classe, porém com o método o this já aponta para classe em questão.
+    static manutencao(){
+      console.log("A manutenção do veículo precisa ser feita!");
+    }
   }
   
   const meuCarro = new Carro('Ford', 'Mustang', 2020);
   meuCarro.apresentar(); 
-  meuCarro.dirigir(); 
+  meuCarro.dirigir();  // E aqui estamos chamando o método instânciado, a partir do que foi criado pela classe.
   meuCarro.parar(); 
   
+  Carro.manutencao(); // Para chamar o método estático vamos direto na classe, pois ele não está associado a instância (objeto criado a partir da clase)
