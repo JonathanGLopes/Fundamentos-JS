f20bef0e525f054bb248a14fa0a6995b7c63eb45
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-03-23 17:20:51 -0300

    Adicionando itens dinâmicamente

--- Lista de compras/main.js
+++ Lista de compras/main.js
@@ -2,6 +2,45 @@ const $novoProduto = document.querySelector(".add");
 const $modal = document.querySelector(".modal");
 const $closeModal = document.querySelector(".fechar");
 const $inputItem = document.querySelector(".item");
+const $lista = document.querySelector(".lista");
+const $addProduto = document.querySelector(".add_item");
+const form = document.querySelector("form");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    isValid();
+})
+
+function isValid(){
+    if($inputItem.value === '') {
+        document.querySelector("span").style.visibility = "visible";
+        $inputItem.focus();
+        return;
+    } else {
+        newLi();
+        addAndClose();
+    }
+}
+
+function addAndClose(){
+    $modal.style.display = 'none';
+    $novoProduto.classList.remove('desabilitado');
+}
+
+function newLi(){  
+    const li = document.createElement('li');
+    li.textContent = $inputItem.value;
+    $lista.appendChild(li);
+
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", "fa-delete-left", "lixo");
+    li.appendChild(icon);
+    $inputItem.value = '';
+    $inputItem.focus();
+    document.querySelector("span").style.visibility = "hidden";
+}
+
 
 function showModal() {
     $novoProduto.addEventListener("click", () => {
