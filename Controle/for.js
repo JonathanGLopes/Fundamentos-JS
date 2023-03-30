9f6b1315b49facdfda87f8e837cb22098913928e
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-03-30 19:21:20 -0300

    organizando pasta

--- Controle/for.js
+++ Controle/for.js
@@ -6,3 +6,16 @@ const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
 for (let i = 0; i < notas.length; i++) {
     console.log(`nota = ${notas[i]}`)
 }
+
+/*Aqui criamos uma função para calcular quantos anos faltam até uma pessoa fazer 18. Como parâmetro da função foi passado a idade atual e em seguida criamos uma estrutura de repetição utilizando o for.
+O laço do for irá incrementar enquanto for menor do que 18, sendo assim com a condição passada basta informarmos a idade atual no parâmetro e obter o resultado.*/
+
+function quantoAte18(idade){
+    for(let i = 0; i < 18; i++){
+        if(idade + i === 18){
+            return i;
+        }
+    }
+}
+
+console.log(quantoAte18(7));
