cc0aad51cced5d0e3625c3b811736593b924fbc5
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-04-03 19:54:05 -0300

    Exemplo POO

---
+++ Objeto/POO.js
@@ -0,0 +1,18 @@
+class Tarefa {
+    constructor(descricao, dataVencimento) {
+      this.descricao = descricao;
+      this.dataVencimento = dataVencimento;
+      this.concluida = false;
+    }
+  
+    marcarConcluida() {
+      this.concluida = true;
+    }
+  
+    adiar(novadataVencimento) {
+      this.dataVencimento = novadataVencimento;
+    }
+  }
+
+const manutencao = new Tarefa('Manutenção em todas máquinas do escritório', '20/04/2023');
+console.log(manutencao);
