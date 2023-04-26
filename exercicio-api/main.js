7f1e9b324a5c0a25c2747eff397aa89f54d890f0
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-04-26 20:44:42 -0300

    inserindo os IDs na tela

--- exercicio-api/main.js
+++ exercicio-api/main.js
@@ -1,5 +1,16 @@
 const content = document.querySelector(".content");
 const inputSearch = document.querySelector(".input");
+content.classList.add("div");
+
+const containerNomes = document.createElement("div");
+containerNomes.classList.add("container");
+content.append(containerNomes);
+
+const containerIds = document.createElement("div");
+containerIds.classList.add("container");
+content.append(containerIds);
+
+
 
 let items = [];
 
@@ -11,17 +22,29 @@ inputSearch.oninput = () => {
     .forEach((item) => addHTML(item));
 }
 
-function addHTML(item){
-    const div = document.createElement("div");
+function addNome(item){
+    const div = document.createElement("span");
+    div.classList.add('nomes');
     div.innerHTML = item;
-    content.append(div);
+    containerNomes.append(div);
+}
+
+function addId(item){
+    const div = document.createElement("span");
+    div.classList.add('ids')
+    div.innerHTML = item;
+    containerIds.append(div);
 }
 
 fetch("https://jsonplaceholder.typicode.com/users")
     .then((data) => data.json())
     .then((users) => {
         users.forEach((user) => {
-            addHTML(user.name);
-            items.push(user.name)
+            addNome(user.name);
+            items.push(user.name);
         });
-    });
+        users.forEach((id) => {
+            addId(id.id);
+            items.push(id.id)
+        });
+    })
