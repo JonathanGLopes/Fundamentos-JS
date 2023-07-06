9d4c9bd3ee3bab44b9f5837a035ba53b931e2fb3
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-07-06 20:08:32 -0300

    att exemplo for in

--- logica/forIn.js
+++ logica/forIn.js
@@ -1,10 +1,13 @@
-// For in = Lê os índices ou chaves do objeto
+// For in representa o indice ou chave do objeto durante cada iteração.
 
 const frutas = ['Pera', 'Maça', 'Uva', 'Laranja'];
 
+//Neste exemplo temos o console log do i que a resposta dele é o indice de cada elemento do array frutas. E o frutas[i] é a sintaxe para obter o valor de cada indice.
 for (let i in frutas){
-    console.log(frutas[i])
+    console.log(`O indice ${i} representa a fruta ${frutas[i]}`)
 }
+
+
 
 const pessoa = {
     nome: 'Jonathan',
@@ -12,6 +15,8 @@ const pessoa = {
     idade: 26
 };
 
+//Aqui nós temos outro console log e no i dele nós temos o retorno de cada chave do objeto pessoa. E no pessoa [i] nós temos o valor de cada chave desse objeto.
+
 for(let i in pessoa){
-    console.log(i, pessoa[i])
+    console.log(`A chave do objeto pessoa é ${i} e seu valor é ${pessoa[i]}`)
 }
