6e2984b135e4798193e34266fe8d594cc2d12306
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-07-09 20:26:55 -0300

    operação ternária

--- logica/operacaoTernaria.js
+++ logica/operacaoTernaria.js
@@ -6,9 +6,13 @@ const pontosUsuario = 999;
 //     console.log('Usuário nroaml');
 // }
 
+let idade = 20;
+const pessoa = idade >= 18 ? 'Adulto' : 'Menor de idade';
+console.log(pessoa)
+
+
 const nivelUsuario = pontosUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
 console.log(nivelUsuario);
-
 
 /*
     No código acima temos em uma linha a mesma funcionalidade do if else comentado, esse é um exemplo de operação ternária para aumentar a produtividade e diminuir linhas de código.
