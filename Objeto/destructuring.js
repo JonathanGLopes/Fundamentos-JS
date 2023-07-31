4fa59a5d5621714c71bd0aba0829485ac8cc8771
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-07-30 22:03:26 -0300

    criando novo valor a partir do destructuring

--- Objeto/destructuring.js
+++ Objeto/destructuring.js
@@ -29,3 +29,7 @@ console.log(localName, OppeningHours, categories);
 //Utilizando outro nome opcional para o valor da chave
 const {localName: nomeRestaurante, OppeningHours: horario} = restaurante;
 console.log(nomeRestaurante, horario);
+
+//Caso a propriedade não exista no objeto, podemos definir um valor padrão para a variável.
+const {supervision = 'aprovado'} = restaurante;
+console.log(supervision);
