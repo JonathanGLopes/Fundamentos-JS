a1674e6740305a05abc85862543c1344f64f70bb
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-08-05 20:56:41 -0300

    pratica map array de objeto

--- Array/map.js
+++ Array/map.js
@@ -2,9 +2,8 @@
 
 const numeros = [5, 25, 10, 30, 40, 50, 60];
 
-const numerosEmDobro = numeros.map(valor => valor * 2);
+const numerosEmDobro = numeros.map((valor) => valor * 2);
 console.log(numerosEmDobro);
-
 
 // Para cada elemento:
 // Retorne apenas uma string com o nome da pessoa
@@ -12,21 +11,20 @@ console.log(numerosEmDobro);
 // Adicione uma chave id em cada objeto
 
 const pessoas = [
-    {nome: 'Luiz', idade: 62},
-    {nome: 'Maria', idade: 23},
-    {nome: 'Eduardo', idade: 55},
-    {nome: 'Letícia', idade: 19},
-    {nome: 'Rosana', idade: 32},
-    {nome: 'Wallace', idade: 47},
-    {nome: 'Alex', idade: 66},
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Letícia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+  { nome: "Alex", idade: 66 },
 ];
 
-const nomes = pessoas.map(function(objeto){
-    return objeto.nome;
+const nomes = pessoas.map(function (objeto) {
+  return objeto.nome;
 });
 
-const idades = pessoas.map(objeto => ({idade: objeto.idade}));
-
+const idades = pessoas.map((objeto) => ({ idade: objeto.idade }));
 
 //Fazendo dessa forma nós iremos alterar o objeto original. Se dermos um log no objeto pessoas, vamos ver que foi adicionado uma chave id a ele.
 // const id = pessoas.map(function(objeto, indice){
@@ -35,33 +33,56 @@ const idades = pessoas.map(objeto => ({idade: objeto.idade}));
 // });
 
 // Se o objetivo não for alterar o objeto original mas sim retornar um novo com uma chave id, podemos fazer conforme o exemplo abaixo.
-const novoId = pessoas.map(function(objeto, indice){
-    const novoObjeto = {...objeto}; /// Spread operator
-    novoObjeto.id = indice + 1;
-    return novoObjeto;
+const novoId = pessoas.map(function (objeto, indice) {
+  const novoObjeto = { ...objeto }; /// Spread operator
+  novoObjeto.id = indice + 1;
+  return novoObjeto;
 });
 
-console.log(novoId)
-
+console.log(novoId);
 
 const meuArray = [
-    [10, 20],
-    [30, 40],
-    [50, 60],
-    [70, 80],
-    [90, 100],
+  [10, 20],
+  [30, 40],
+  [50, 60],
+  [70, 80],
+  [90, 100],
 ];
-
 
 // Neste exemplo o valor representado pelo item na função é o próprio array, pois como podemos ver, o meuArray contém vários arrays dentro dele, então o subItem que fica responsável pelo valor de cada array.
 
-const novoArray = meuArray.map(function(item){
-    let soma = 0;
-    item.forEach(function(subItem){
-        soma += subItem;
-    });
+const novoArray = meuArray.map(function (item) {
+  let soma = 0;
+  item.forEach(function (subItem) {
+    soma += subItem;
+  });
 
-    return soma;
+  return soma;
 });
 
 console.log(novoArray);
+
+// Retornando os títulos de cada objeto
+const livros = [
+  {
+    titulo: "Harry potter",
+    genero: "Aventura",
+    indicacao: "A partir de 14 anos",
+  },
+  {
+    titulo: "Sherlock Holmes",
+    genero: "Mistério/Investigação",
+    indicacao: "A partir de 16 anos",
+  },
+  {
+    titulo: "O pequeno príncipe",
+    genero: "Conto de fadas",
+    indicacao: "A partir de 8 anos",
+  },
+];
+
+const tituloLivros = livros.map((livro) => {
+  return livro.titulo;
+});
+
+console.log(tituloLivros);
