3dcb61921ab003d980b37bd07968c24d55de4c6e
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-10-13 20:34:49 -0300

    spread operator

--- Array/spread.js
+++ Array/spread.js
@@ -1,4 +1,5 @@
 // Spread Operator
+// Ele tem a função de expandir os dados dentro de um outro elemnto iterável, como arrays e objetos.
 
 let primeiro = [1, 2, 3, 4, 5];
 
@@ -11,19 +12,18 @@ console.log(segundo);
 let segundoOperator = [...primeiro, 6, 7, 8, 9, 10];
 console.log(segundoOperator);
 
-
 const pessoa = {
-    nome: 'Jorge',
-    sobrenome: 'Menezes',
-    idade: 43,
-}
+  nome: "Jorge",
+  sobrenome: "Menezes",
+  idade: 43,
+};
 
 const info = {
-    ...pessoa,
-    cargo: 'Gerente de projetos',
-    codigo: 536597,
-    telefone: 1155555555
-}
+  ...pessoa,
+  cargo: "Gerente de projetos",
+  codigo: 536597,
+  telefone: 1155555555,
+};
 
 // O mesmo funciona com objetos, como podemos ver que o console devolveu as informações de ambos objetos apenas no info, também contendo as informações do pessoa
 console.log(info);
