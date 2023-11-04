1756257ecd7da99bcc5246c70b195ea839efe767
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-11-03 22:04:47 -0300

    intro array.from

---
+++ Array/from.js
@@ -0,0 +1,13 @@
+const numeros = Array.from({ length: 10 }, (_, i) => i + 1);
+
+console.log(numeros);
+
+const numeroInicial = 50;
+const tamanhoLista = 21;
+
+listaNumeros = Array.from(
+  { length: tamanhoLista },
+  (_, i) => numeroInicial + i
+);
+
+console.log(listaNumeros);
