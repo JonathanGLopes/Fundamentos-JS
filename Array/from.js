c6383f457a7447940cdecf23473ce36084c19055
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-11-04 23:07:36 -0300

    conceito array.from

--- Array/from.js
+++ Array/from.js
@@ -1,3 +1,6 @@
+//O Array.from é um método em JavaScript que é usado para criar um novo array a partir de um objeto que seja "iterável"
+//(ou seja, que pode ser percorrido em um loop, como uma string, um array-like object, um mapa, um conjunto, etc.).
+
 const numeros = Array.from({ length: 10 }, (_, i) => i + 1);
 
 console.log(numeros);
